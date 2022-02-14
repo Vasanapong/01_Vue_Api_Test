33193c34dea4ececc08df4e054fdbf3f0bb22962
Vasanapong <vasanapong.c@gmail.com>
2022-02-14 13:18:36 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ApiTest/>
+</template>
+
+<script>
+import ApiTest from './components/ApiTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ApiTest
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing:border-box;
+  margin:0px;
+  padding: 0px;
+}
+</style>
---
+++ src/components/ApiTest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="api-test">
+    <div>This Is Api Test Component</div>
+    {{userData}}
+  </div>
+</template>
+
+<script>
+export default {
+    name:"ApiTest",
+    data(){
+        return{
+            userData : {}
+        }
+    },
+    methods:{
+        async fetchUser(){
+            let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+            let data = await response.json()
+            this.userData = data
+        }
+    },
+    beforeMount(){
+        this.fetchUser()
+    }
+}
+</script>
+
+<style>
+
+</style>
